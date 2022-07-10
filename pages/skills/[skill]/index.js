2673d152c83366fc580fcca59d50b1d2c31e89b8
naveen8801 <naveensharma10d@gmail.com>
2022-07-10 10:38:21 +0530

    routing using Link

--- pages/skills/[skill]/index.js
+++ pages/skills/[skill]/index.js
@@ -1,7 +1,15 @@
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function () {
   const router = useRouter();
   const skill = router.query.skill;
-  return <div>Skill is {skill}</div>;
+  return (
+    <div>
+      <div>Skill is {skill}</div>
+      <Link href={`/skills/${skill}/web-dev`}>
+        <a>Project</a>
+      </Link>
+    </div>
+  );
 }
